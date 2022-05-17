15ed43c0a4a253b3e633f5953ab709c4036a0bd4
Max Wang <xw1499@hlog-2.hpc.nyu.edu>
2022-05-17 12:46:39 -0400

    Using our own test train split

--- ext1_xx.py
+++ ext1_xx.py
@@ -16,14 +16,14 @@ import pandas as pd
 import time
 %matplotlib inline
 
-# read in small dataset
-data_small = pd.read_csv('/Users/Xiu/Documents/dsga1004/final-project-group_80/ml-latest-small/ratings.csv')[['userId', 'movieId', 'rating']]
-#val_small   = pd.read_csv('/Users/Xiu/Documents/dsga1004/final-project-group_80/val_small.csv')
-#test_small  = pd.read_csv('/Users/Xiu/Documents/dsga1004/final-project-group_80/test_small.csv')[['userId', 'movieId', 'rating']]
+# read in small dataset (rememeber to adjust the path)
+#data_small = pd.read_csv('/Users/Xiu/Documents/dsga1004/final-project-group_80/ml-latest-small/ratings.csv')[['userId', 'movieId', 'rating']]
+train_small   = pd.read_csv('/Users/Xiu/Documents/dsga1004/final-project-group_80/train_small.csv')[['userId', 'movieId', 'rating']]
+test_small  = pd.read_csv('/Users/Xiu/Documents/dsga1004/final-project-group_80/test_small.csv')[['userId', 'movieId', 'rating']]
 
 # rename cols
-data_small.rename(columns = {'userId':'user', 'movieId':'item'}, inplace = True)
-#test_small.rename(columns = {'userId':'user', 'movieId':'item'}, inplace = True)
+train_small.rename(columns = {'userId':'user', 'movieId':'item'}, inplace = True)
+test_small.rename(columns = {'userId':'user', 'movieId':'item'}, inplace = True)
 
 algo_als = als.BiasedMF(10, iterations=3, reg=0.1) #tune later according to the other model
 
@@ -41,12 +41,9 @@ def eval(aname, algo, train, test):
     return recs
 
 all_recs = []
-test_small = []
 #Start timer here
 tic = time.perf_counter()
-for train, test in xf.partition_users(data_small, 5, xf.SampleFrac(0.2)):
-    test_small.append(test)
-    all_recs.append(eval('ALS', algo_als, train, test))
+all_recs.append(eval('ALS', algo_als, train_small, test_small))
 #End timer here 
 toc = time.perf_counter() 
 print(f"Finished program in {toc - tic:0.4f} seconds")
@@ -54,14 +51,12 @@ all_recs = pd.concat(all_recs, ignore_index=True)
 all_recs.head()
 
 all_recs.shape
-
 test_small = pd.concat(test_small, ignore_index=True)
 
 rla = topn.RecListAnalysis()
 rla.add_metric(topn.precision)
 results = rla.compute(all_recs, test_small)
 results.head()
-results.to_csv('extention1_result/precision_small.csv')
 
 
 
